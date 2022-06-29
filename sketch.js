5a4f00d38bb82b0eaa91baa4fcf0329ede000762
swiftie#13akaRITI <67743556+riti2009@users.noreply.github.com>
2022-06-29 20:19:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var s1, s2, s3
+function setup() {
+  createCanvas(400,400);
+s1 = new Student ('Sophie', 13, 'Level 3');
+s2 = new Student ('Keefe', 14, 'Level 5');
+s3 = new Student ('Biana', 13, 'Level 3');
+s1.show()
+s2.show()
+s3.show()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
---
+++ student.js
@@ -0,0 +1,20 @@
+
+class Student
+{
+ constructor(n,a,g){
+    this.name = n;
+    this.age = a;
+    this.grade = g;
+ }  
+show(){
+    console.log(this.name, this.age, this.grade);
+}
+
+
+
+}
+
+
+
+
+
